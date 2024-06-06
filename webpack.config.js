abdd991117d868759df791db9d7d0b502f1ab6f1
Xi <xiarx@protonmail.com>
2024-06-06 19:28:34 +0200

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,71 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+import path from 'path'
+import {fileURLToPath} from 'url'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import WorkboxWebpackPlugin from 'workbox-webpack-plugin'
 
-const isProduction = process.env.NODE_ENV == "production";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const isProduction = process.env.NODE_ENV == 'production'
 
-const stylesHandler = "style-loader";
+const stylesHandler = 'style-loader'
 
 const config = {
-  entry: "./src/index.ts",
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: 'index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
+        use: [stylesHandler, 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
+        use: [stylesHandler, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+    alias: {
+      '@app': path.resolve(__dirname, 'src'),
+    },
   },
-};
+}
 
-module.exports = () => {
+if (!isProduction) {
+  config.devServer = {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 4000,
+  }
+}
+
+export default () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production';
 
     config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
   } else {
-    config.mode = "development";
+    config.mode = 'development';
   }
   return config;
-};
+}
